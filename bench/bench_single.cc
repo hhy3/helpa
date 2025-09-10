6d4bb14f7cd2f35e0b0dfb37d49ac452dcc4e449
zh Wang <rekind133@outlook.com>
2025-09-10 19:17:37 +0800

    update

--- bench/bench_single.cc
+++ bench/bench_single.cc
@@ -24,29 +24,16 @@ bench_l2_fp32_fp32(benchmark::State& s) {
 }
 
 static void
-bench_l2_u7_u7(benchmark::State& s) {
-    std::vector<uint8_t> x(dim);
-    std::vector<uint8_t> y(dim);
-    std::mt19937 rng;
-    std::uniform_int_distribution<uint8_t> dis(0, 127);
-    std::generate(x.begin(), x.end(), [&] { return dis(rng); });
-    std::generate(y.begin(), y.end(), [&] { return dis(rng); });
-    check_int32_equal(helpa::l2_u7_u7_ref(x.data(), y.data(), dim), helpa::l2_u7_u7(x.data(), y.data(), dim));
-    for (auto _ : s) {
-        benchmark::DoNotOptimize(helpa::l2_u7_u7(x.data(), y.data(), dim));
-    }
-}
-
-static void
-bench_l2_s8_s8(benchmark::State& s) {
+bench_l2_s7_s7(benchmark::State& s) {
     std::vector<int8_t> x(dim);
     std::vector<int8_t> y(dim);
     std::mt19937 rng;
-    std::uniform_int_distribution<int8_t> dis(-127, 127);
+    std::uniform_int_distribution<int8_t> dis(0, 127);
     std::generate(x.begin(), x.end(), [&] { return dis(rng); });
     std::generate(y.begin(), y.end(), [&] { return dis(rng); });
+    check_int32_equal(helpa::l2_s7_s7_ref(x.data(), y.data(), dim), helpa::l2_s7_s7(x.data(), y.data(), dim));
     for (auto _ : s) {
-        benchmark::DoNotOptimize(helpa::l2_s8_s8(x.data(), y.data(), dim));
+        benchmark::DoNotOptimize(helpa::l2_s7_s7(x.data(), y.data(), dim));
     }
 }
 
@@ -79,56 +66,7 @@ bench_dot_fp32_fp32(benchmark::State& s) {
     }
 }
 
-static void
-bench_dot_u8_s8(benchmark::State& s) {
-    std::vector<uint8_t> x(dim);
-    std::vector<int8_t> y(dim);
-    std::mt19937 rng;
-    std::uniform_int_distribution<uint8_t> udis(0, 255);
-    std::uniform_int_distribution<int8_t> sdis(-128, 127);
-    std::generate(x.begin(), x.end(), [&] { return udis(rng); });
-    std::generate(y.begin(), y.end(), [&] { return sdis(rng); });
-    check_int32_equal(helpa::dot_u8_s8_ref(x.data(), y.data(), dim), helpa::dot_u8_s8(x.data(), y.data(), dim));
-    for (auto _ : s) {
-        benchmark::DoNotOptimize(helpa::dot_u8_s8(x.data(), y.data(), dim));
-    }
-}
-
-static void
-bench_dot_s8_s8(benchmark::State& s) {
-    std::vector<int8_t> x(dim);
-    std::vector<int8_t> y(dim);
-    std::mt19937 rng;
-    std::uniform_int_distribution<int8_t> dis(-127, 127);
-    std::generate(x.begin(), x.end(), [&] { return dis(rng); });
-    std::generate(y.begin(), y.end(), [&] { return dis(rng); });
-    check_int32_equal(helpa::dot_s8_s8_ref(x.data(), y.data(), dim), helpa::dot_s8_s8(x.data(), y.data(), dim));
-
-    for (auto _ : s) {
-        benchmark::DoNotOptimize(helpa::dot_s8_s8(x.data(), y.data(), dim));
-    }
-}
-
-static void
-bench_dota_u4_u4(benchmark::State& s) {
-    std::vector<uint8_t> x(dim / 2);
-    std::vector<uint8_t> y(dim / 2);
-    std::mt19937 rng;
-    std::uniform_int_distribution<uint8_t> dis(0, 255);
-    std::generate(x.begin(), x.end(), [&] { return dis(rng); });
-    std::generate(y.begin(), y.end(), [&] { return dis(rng); });
-    check_int32_equal(helpa::dot_u4_u4_ref(x.data(), y.data(), dim), helpa::dota_u4_u4(x.data(), y.data(), dim));
-
-    for (auto _ : s) {
-        benchmark::DoNotOptimize(helpa::dota_u4_u4(x.data(), y.data(), dim));
-    }
-}
-
 BENCHMARK(bench_l2_fp32_fp32);
-BENCHMARK(bench_l2_u7_u7);
-BENCHMARK(bench_l2_s8_s8);
+BENCHMARK(bench_l2_s7_s7);
 BENCHMARK(bench_l2a_u4_u4);
 BENCHMARK(bench_dot_fp32_fp32);
-BENCHMARK(bench_dot_u8_s8);
-BENCHMARK(bench_dot_s8_s8);
-BENCHMARK(bench_dota_u4_u4);
